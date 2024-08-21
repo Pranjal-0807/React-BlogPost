8badd13b393aa30c7c342e679f6278de1f86c222
Pranjal-0807 <pranjalagarwak107@gmail.com>
2024-08-22 01:01:11 +0530

    Added Home and About Component

---
+++ blogpost/src/Components/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <>
+      <section>
+        <h2>About the Author</h2>
+        <p>
+          This website is currently being built up by the students of MIT,
+          Moradabad
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default About;
