9fbb8ef0b610976a2ee1f6cc05aeb54038b60183
Pranjal-0807 <pranjalagarwak107@gmail.com>
2024-08-22 00:41:54 +0530

    Added Navbar and Footer Component

---
+++ blogpost/src/Components/NavBar.jsx
@@ -0,0 +1,25 @@
+const NavBar = () => {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="site-title">MIT BLOGPOSTS</div>
+        <ul>
+          <li>
+            <a href="/blogs">Blogs</a>
+          </li>
+          <li>
+            <a href="/blogs/form">New Blog</a>
+          </li>
+          <li>
+            <a href="/auth/login">Login</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
